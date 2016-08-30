1abf0d621df0adfdb3d66d4083f8dcf26909154e
Karol Kowalski <ordon1313@gmail.com>
2016-08-30 15:50:03 +0200

    refactor

--- src/main/java/plyglot/dojo/compressor/Compressor.kt
+++ src/main/java/plyglot/dojo/compressor/Compressor.kt
@@ -4,14 +4,23 @@ fun compress(input: String): String {
     return if (input.length <= 1) {
         input
     } else if (hasSequence(input)) {
-        var sequenceLength = 0
-        input.toCharArray().forEach {
-            if(it == input[0]) sequenceLength += 1 else return@forEach
-        }
+        val sequenceLength = squenceLenght(input)
         sequenceLength.toString() + input[0] + compress(input.substring(sequenceLength))
     } else {
         input[0] + compress(input.substring(1))
     }
+}
+
+fun squenceLenght(input: String) : Int{
+    var sequenceLength = 0
+    input.toCharArray().forEach {
+        if(it == input[0]) {
+            sequenceLength += 1
+        } else {
+            return@forEach
+        }
+    }
+    return sequenceLength
 }
 
 fun hasSequence(input: String): Boolean {
