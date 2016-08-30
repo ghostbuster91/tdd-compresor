4a9b414cdd1cbbd76ae63d9b90c91712778342ff
Karol Kowalski <ordon1313@gmail.com>
2016-08-30 15:30:32 +0200

    refactor

--- src/main/java/plyglot/dojo/compressor/Compressor.kt
+++ src/main/java/plyglot/dojo/compressor/Compressor.kt
@@ -1,6 +1,10 @@
 package plyglot.dojo.compressor
 
 fun compress(input: String): String {
-    return if (input.length > 1 && input[0] == input[1]) "2" + input[0] else input
+    return if (input.length > 1 && input[0] == input[1]) {
+        "2" + input[0]
+    } else {
+        input
+    }
 }
 
